630d00038e44856c0549ea6516c1d797b26aa6e8
Junchen01 <jub424@lehigh.edu>
2022-11-30 03:48:52 -0500

    board card excahage and select work now

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,17 @@
-var suits = ["s", "h", "c", "d"];
-var values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
-var deck = new Array();
+let suits = ["s", "h", "c", "d"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
+
+let deck;
+let playercount = 0;
+let hands = [];
+let board = [];
+let onSelect = false;
+let onSelectObject = null;
 
 function createDeck(){
-    deck = new Array();
-    for (var i = 0 ; i < values.length; i++){
-        for(var j = 0; j < suits.length; j++)
-        {
+    let deck = new Array();
+    for(var j = 0; j < suits.length; j++){
+        for (var i = 0 ; i < values.length; i++){
             var weight = parseInt(values[i]);
             if (values[i] == "T" || values[i] == "J" || values[i] == "Q" || values[i] == "K"){
                 weight = 10;
@@ -14,28 +19,152 @@ function createDeck(){
             if (values[i] == "A"){
                 weight = 11;
             }
-            let path = "img/" + values[i] + suits[j] + ".png";
+            let path = "card/" + values[i] + suits[j] + ".png";
             var card = { value: values[i], suit: suits[j], weight: weight, path: path };
             deck.push(card);
         }
     }
-    shuffleArray(array);
+    return deck;
 }
 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+function displayDeck(deck){
+    for(let i = 0; i < deck.length; i++){
+        let img_tmp = '<img src="' + deck[i].path + '"';
+        img_tmp += 'alt="' + deck[i].value + deck[i].suit + '"';
+        img_tmp += 'id="' + deck[i].value + deck[i].suit + '"';
+        img_tmp += 'class="selectable"';
+        img_tmp += 'width="50"';
+        img_tmp += 'onclick="selectDeck(this)">';
+        $("#deck").append(img_tmp);
     }
 }
 
-createDeck();
-console.log(deck);
+function addPlayer(){
+    playercount++;
+    let player_tmp = '<div id="' + playercount + '">';
+    player_tmp += '<h3>Player ' + playercount + ':</h3>';
+    player_tmp += '<img src="card/back.png" alt="Girl in a jacket" width="75">';
+    player_tmp += '<img src="card/back.png" alt="Girl in a jacket" width="75">';
+    player_tmp += '</div>'; 
+    $("#players").append(player_tmp);
+    hands.push[null,null];
+}
 
+function removePlayer(){
+    $("#"+ playercount).remove();
+    hands.pop();
+    playercount--;
+}
+
+function setupBoard(){
+    for(let i = 0; i < 5; i++){
+        let boardCard_tmp = '<img src="card/back.png"';
+        boardCard_tmp += 'alt="empty card"';
+        boardCard_tmp += 'id="board"';
+        boardCard_tmp += 'width="75"';
+        boardCard_tmp += 'class="empty board"';
+        boardCard_tmp += 'onclick="selectCard(this)">';
+        $("#board").append(boardCard_tmp);
+    }
+}
+
+function selectCard(img){
+    if(img.classList.contains('selected')){
+        img.src = 'card/back.png';
+        document.getElementById(img.alt).classList.remove("unselectable");
+        document.getElementById(img.alt).classList.add("selectable");
+        img.classList.add('empty');
+        img.classList.remove('selected');
+        img.alt = 'empty card';
+    }else{
+        if(onSelectObject === img){
+            onSelectObject = null;
+            onSelect = false;
+            img.style.border = '';
+        }else{
+            if(onSelectObject !== null){
+                onSelectObject.style.border = '';
+            }
+            onSelectObject = img;
+            img.style.border = "3px solid black";
+            onSelect = true;
+        }
+    }
+}
+
+function selectDeck(img){
+    if(onSelect && img.classList.contains('selectable')){
+        onSelectObject.src = img.src;
+        onSelectObject.alt = img.id;
+        onSelectObject.classList.remove("empty");
+        onSelectObject.classList.add("selected");
+        img.classList.remove("selectable");
+        img.classList.add("unselectable");
+        onSelectObject.style.border = '';
+        onSelectObject = null;
+        onSelect = false;
+    }
+}
+
+function setupPlayers(){
+    for(let i = 0; i < 2; i++){
+        addPlayer();
+    }
+}
+
+$("#deck").empty().appendTo("hi");
 
 $(() => {
-    
-});
+    deck = createDeck();
+    displayDeck(deck);
+    setupBoard();
+    setupPlayers();
 
+    $("#add").unbind().click(() => {
+        if(playercount < 9){
+            addPlayer();
+        }else{
+            alert("at MOST NINE player on table")
+        }
+    });
+
+    $("#remove").unbind().click(() => {
+        if(playercount > 2){
+            removePlayer();
+        }else{
+            alert("at LEAST TWO player on table")
+        }
+    });
+
+    $("#new").unbind().click(() => {
+        board = [];
+        $('.board').each(function() {
+            if($(this).attr("alt") !== 'empty card'){
+                board.push($(this).attr("alt"));
+            }
+        });
+        console.log(board)
+    });
+
+    $("#calculate").unbind().click(() => {
+        $.ajax(
+            "/calculate",
+            {
+              type: "GET",
+              data: {
+                hands: hands,
+                board: board
+              },
+              dataType: "text",
+              success: function (json) {
+                $("#tableBody").html(json)
+              },
+              error: function (jqXHR, textStatus, errorThrown) {
+                alert("Error: " + jqXHR.responseText);
+                alert("Error: " + textStatus);
+                alert("Error: " + errorThrown);
+              }
+            }
+          )
+    });
+});
--- server.js
+++ server.js
@@ -5,14 +5,33 @@
  *  assignment number: Final project
  */
 
+import { calculateEquity } from 'poker-odds'
+import express from 'express'
+import path from 'path'
 
- const express = require("express");
- const path = require("path");
- 
- let app = express();
- 
- let publicPath = path.resolve(__dirname, "public");
- app.use(express.static(publicPath));
- 
- 
- app.listen(3000, () => console.log("Starting up poker game"));
+let app = express();
+
+let publicPath = path.resolve(process.cwd(), "public");
+console.log(publicPath);
+app.use(express.static(publicPath));
+
+
+function cal_props(hands,board){
+    const iterations = 100000 // optional
+    const exhaustive = false // optional
+    let output = calculateEquity(hands, board, iterations, exhaustive);
+    return output;
+}
+
+const hands = [['As', 'Kh'], ['Kd', 'Qs'],['Js','Jh']]
+const board = []
+
+
+let output = cal_props(hands,board)
+console.log(output)
+
+
+
+
+
+app.listen(3000, () => console.log("Starting up poker game"));
