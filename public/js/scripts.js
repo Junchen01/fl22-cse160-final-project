b261fa13d3acef0c8363f8db3d9d3bd75e07ea66
JunchenBao <jub424@lehigh.edu>
2022-11-30 16:04:17 -0500

    routes set up, can cal prob now

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 let suits = ["s", "h", "c", "d"];
-let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
+let values = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 
 let deck;
 let playercount = 0;
@@ -7,6 +7,9 @@ let hands = [];
 let board = [];
 let onSelect = false;
 let onSelectObject = null;
+
+let checkempty = true;
+let checkboard = true;
 
 function createDeck(){
     let deck = new Array();
@@ -28,15 +31,22 @@ function createDeck(){
 }
 
 function displayDeck(deck){
-    for(let i = 0; i < deck.length; i++){
-        let img_tmp = '<img src="' + deck[i].path + '"';
-        img_tmp += 'alt="' + deck[i].value + deck[i].suit + '"';
-        img_tmp += 'id="' + deck[i].value + deck[i].suit + '"';
-        img_tmp += 'class="selectable"';
-        img_tmp += 'width="50"';
-        img_tmp += 'onclick="selectDeck(this)">';
-        $("#deck").append(img_tmp);
+    let i = 0;
+    let img_tmp = '';
+    for(let j = 0; j < 4; j++){
+        img_tmp += '<tr>';
+        for(let k = 0; k < 13; k++){
+            img_tmp += '<td><img src="' + deck[i].path + '"';
+            img_tmp += 'alt="' + deck[i].value + deck[i].suit + '"';
+            img_tmp += 'id="' + deck[i].value + deck[i].suit + '"';
+            img_tmp += 'class="selectable"';
+            img_tmp += 'width="50"';
+            img_tmp += 'onclick="selectDeck(this)"></td>';
+            i++;
+        }
+        img_tmp += '</tr>';
     }
+    $("#deck").append(img_tmp);
 }
 
 function addPlayer(){
@@ -48,11 +58,10 @@ function addPlayer(){
         player_tmp += 'alt="empty card"';
         player_tmp += 'id="palyer' + playercount + '-' + (i+1) + '"';
         player_tmp += 'width="75"';
-        player_tmp += 'class="empty player1"';
+        player_tmp += 'class="empty player' + playercount + '"';
         player_tmp += 'onclick="selectCard(this)">';
     }
     $("#players").append(player_tmp);
-    hands.push[null,null];
 }
 
 function removePlayer(){
@@ -117,6 +126,49 @@ function setupPlayers(){
     }
 }
 
+function getBoardCard(){
+    board = [];
+    $('.board').each(function() {
+        if($(this).attr("alt") !== 'empty card'){
+            board.push($(this).attr("alt"));
+        }
+    });
+    if(board.length === 1 || board.length === 2){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function getPlayersCard(){
+    checkempty = true;
+    hands = [];
+    for(let i = 0; i < playercount; i++){
+        let playerCard_tmp = [];
+        $('.player' + (i+1)).each(function() {
+            if($(this).attr("alt") !== 'empty card'){
+                playerCard_tmp.push($(this).attr("alt"));
+            }else{
+                checkempty = false;
+            }
+        });
+        hands.push(playerCard_tmp);
+    }
+    return checkempty;
+}
+
+function getCardArrays(){
+    if(!getBoardCard()){
+        alert('must be the situation of PREFLOP, FLOP, TURN or RIVER\n(0/3/4/5 cards on the board)');
+        return false;
+    }else if(!getPlayersCard()){
+        alert('must fill out ALL players card!');
+        return false;
+    }else{
+        return true;
+    }
+}
+
 $("#deck").empty().appendTo("hi");
 
 $(() => {
@@ -142,34 +194,31 @@ $(() => {
     });
 
     $("#new").unbind().click(() => {
-        board = [];
-        $('.board').each(function() {
-            if($(this).attr("alt") !== 'empty card'){
-                board.push($(this).attr("alt"));
-            }
-        });
-        console.log(board)
+        
     });
 
     $("#calculate").unbind().click(() => {
-        $.ajax(
-            "/calculate",
-            {
-              type: "GET",
-              data: {
-                hands: hands,
-                board: board
-              },
-              dataType: "text",
-              success: function (json) {
-                $("#tableBody").html(json)
-              },
-              error: function (jqXHR, textStatus, errorThrown) {
-                alert("Error: " + jqXHR.responseText);
-                alert("Error: " + textStatus);
-                alert("Error: " + errorThrown);
-              }
-            }
-          )
+        if(getCardArrays()){
+            $.ajax(
+                "/calculate",
+                {
+                  type: "GET",
+                  data: {
+                    hands: hands,
+                    board: board
+                  },
+                  dataType: "text",
+                  success: function (json) {
+                    console.log("hi")
+                    console.log(JSON.parse(json));
+                  },
+                  error: function (jqXHR, textStatus, errorThrown) {
+                    alert("Error: " + jqXHR.responseText);
+                    alert("Error: " + textStatus);
+                    alert("Error: " + errorThrown);
+                  }
+                }
+              )
+        }
     });
 });
--- server.js
+++ server.js
@@ -12,25 +12,51 @@ import path from 'path'
 let app = express();
 
 let publicPath = path.resolve(process.cwd(), "public");
-console.log(publicPath);
 app.use(express.static(publicPath));
 
 
-function cal_props(hands,board){
+function cal_hands(hands,board){
     const iterations = 100000 // optional
     const exhaustive = false // optional
-    let output = calculateEquity(hands, board, iterations, exhaustive);
-    return output;
+    let hands_output = calculateEquity(hands, board, iterations, exhaustive);
+    let probs_output = cal_probs(hands_output);
+    return probs_output;
 }
 
-const hands = [['As', 'Kh'], ['Kd', 'Qs'],['Js','Jh']]
+function cal_probs(hands_output){
+    let probs_output= []
+    for(let i = 0; i < hands_output.length; i++){
+        let total_hand = hands_output[i].count;
+        let win_rate = (hands_output[i].wins/total_hand).toFixed(2) + '%';
+        let tie_rate = (hands_output[i].ties/total_hand).toFixed(2) + '%';
+        let handProbs = [];
+        for(let j = 0; j < hands_output[i].handChances.length; j++){
+            let name_temp = hands_output[i].handChances[j].name;
+            let count_temp = hands_output[i].handChances[j].count;
+            let prob_tmp = (count_temp/total_hand).toFixed(2) + '%';;
+            handProbs.push({name: name_temp, prob: prob_tmp});
+        }
+        let player = { count: total_hand, win: win_rate, tie: tie_rate, handProbs: handProbs};
+        probs_output.push(player);
+    }
+    return probs_output;
+}
+
+app.get("/calculate", function(req, res) {
+    console.log(req.query.board);
+    console.log(req.query.hands);
+    console.log("---------------");
+
+    let hands = req.query.hands;
+    let board = req.query.board;
+    
+    let output = cal_hands(hands,board)
+    let output_json = JSON.stringify(output);
+    res.end(output_json);
+});
+
+const hands = [['As', 'Kh'], ['Kd', 'Qd']]
 const board = []
-
-
-let output = cal_props(hands,board)
-console.log(output)
-
-
 
 
 
